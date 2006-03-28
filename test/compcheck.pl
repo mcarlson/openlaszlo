f3752e1a2b8a238195cd869f58d2048a64cf4938
perforce <perforce@fa20e4f9-1d0a-0410-b5f3-dc9c16b8b17c>
2006-03-28 00:57:24 +0000

    Change:	40922
    User:	mdavis
    
    Description:
    	integrate lps-3.2 deltas to lps-dev
    	minor changes made to the release brach at the last minute
    	only changes to release notes and tests 
    	technical reviewer: none 
    	qa reviewer: none 
    	doc reviewer: none
    	
    	Change approved by Jim Grandy after reviewing scope
    
    git-svn-id: http://svn.openlaszlo.org/openlaszlo/trunk@232 fa20e4f9-1d0a-0410-b5f3-dc9c16b8b17c

--- test/compcheck.pl
+++ test/compcheck.pl
@@ -15,14 +15,22 @@ close IN;
 #$reqTypes = { 'Wrapper' => 'lzt=v1',
 #	    };
 
+#print getstore('http://localhost:8080/lps-3.2/docs/design/programs/laszlo_basics-$7.lzx?lzt=v1','foo.html');
+#print getstore('http://localhost:8080/lps-3.2/demos/calendar/calendar.lzx?lzt=v1','foo.html');
+
+    my $baseURL = 'http://localhost:8080/lps-3.2';
+
 foreach $file (@fileList){
     chomp $file;
+    $file =~ s/"//g;
+    $file =~ s/,//g;
+    # print '"'.$baseURL.$file.'"' ."\n";
+
     $filename = $file;
     $filename =~ s/\//_/g;
     $filename =~ s/\$/D/g;
     print localtime(time).' Storing: new_golden/'.$filename.'_lzt_v1 ... ';
-    $returnCode = getstore('http://localhost:8080/lps-dev'.$file.
-			   '?lzt=v1','new_golden/'.$filename.'_lzt_v1');
+    $returnCode = getstore($baseURL.$file.'?lzt=v1','new_golden/'.$filename.'_lzt_v1');
 
     if ($returnCode == 200 ){
       print "\033[0;32mPASS:\033[0;m Wrapper get of ".$file." completed \n"; 
@@ -30,7 +38,7 @@ foreach $file (@fileList){
       print "\033[1;31mERROR:\033[0;m wrapper get of ".$file.' returned '.$returnCode."\n\n"; 
     }
 
-    $returnCode = getstore('http://localhost:8080/lps-dev'.$file.
+    $returnCode = getstore($baseURL.$file.
 	       '?lzt=swf&lzr=swf6','new_golden/'.$filename.'_lzt_swf6');
 
     if ($returnCode == 200 ){
@@ -39,7 +47,7 @@ foreach $file (@fileList){
       print "\033[1;31mERROR:\033[0;m swf6 get of ".$file.' returned '.$returnCode."\n\n"; 
     }
 
-    $returnCode = getstore('http://localhost:8080/lps-dev'.$file.
+    $returnCode = getstore($baseURL.$file.
 	       '?lzt=swf&lzr=swf7','new_golden/'.$filename.'_lzt_swf7');
     if ($returnCode == 200 ){
       print "\033[0;32mPASS:\033[0;m swf7 get of ".$file." completed \n"; 
@@ -47,3 +55,4 @@ foreach $file (@fileList){
       print "\033[1;31mERROR:\033[0;m swf7 get of ".$file.' returned '.$returnCode."\n\n"; 
     }
 }
+# copyright 2006 Laszlo Systems INC use subject to license
