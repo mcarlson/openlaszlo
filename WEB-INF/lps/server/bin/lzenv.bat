d7373881a1eb4d14a6076db3a73053e536b6ec8b
jgrandy <jgrandy@fa20e4f9-1d0a-0410-b5f3-dc9c16b8b17c>
2006-05-25 23:50:38 +0000

    Summary: Bring over a couple of changes from lps-3.3
    Bugs Fixed:Update version number in build.properties, bring over fix to lfc.bat
    Technical Reviewer: jgrandy
    QA Reviewer: (pending)
    Doc Reviewer: (pending)
    
    
    git-svn-id: http://svn.openlaszlo.org/openlaszlo/trunk@590 fa20e4f9-1d0a-0410-b5f3-dc9c16b8b17c

--- WEB-INF/lps/server/bin/lzenv.bat
+++ WEB-INF/lps/server/bin/lzenv.bat
@@ -3,11 +3,9 @@ REM
 REM lzenv.bat - script to set up laszlo env vars
 REM
 REM * R_LZ_COPYRIGHT_BEGIN ***************************************************
-REM * Copyright 2001-2005 Laszlo Systems, Inc.  All Rights Reserved.         *
+REM * Copyright 2001-2006 Laszlo Systems, Inc.  All Rights Reserved.         *
 REM * Use is subject to license terms.                                       *
 REM * R_LZ_COPYRIGHT_END *****************************************************
-
-set LPS_HOME=c:\Program Files\Laszlo Presentation Server 3.0b2\Server\lps-3.0b2
 
 set LZCP=%LPS_HOME%\WEB-INF\lps\server\build
 for /f "usebackq delims=" %%d in (`dir /s /b "%LPS_HOME%\3rd-party\jars\dev\*.jar"`) do call :add %%~sd
