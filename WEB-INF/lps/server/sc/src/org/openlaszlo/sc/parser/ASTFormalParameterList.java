4be3eeacc9649561729ff436cf25c48a5fbca31b
dda <dda@fa20e4f9-1d0a-0410-b5f3-dc9c16b8b17c>
2007-12-08 15:58:41 +0000

    Change 20071208-dda-S by dda@freddie.local on 2007-12-08 09:57:44 EST     in /Users/dda/laszlo/src/svn/openlaszlo/trunk-b     for http://svn.openlaszlo.org/openlaszlo/trunk
    Summary: Allow type info for functions: Added missing file to last commit
    
    New Features: none
    
    Bugs Fixed: LPP-5059
    
    Technical Reviewer: ptw
    QA Reviewer: promanik
    Doc Reviewer: (pending)
    
    Documentation: none
    
    Release Notes: none
    
    Details:
        Left out a new file from the previous review/commit.
        This was a generated file from javacc, that now is not generated (we override it) as we needed a new method
        to hold the return type.  Sadly, I did not wait for the QA review last time, which would have caught it.
        Missing this file breaks the build.
    
        See history of LPP-5059 for more details.
    
    Tests:
        smoke/smokecheck.lzx
        comparing compiler output files (pending)
    
    
    
    git-svn-id: http://svn.openlaszlo.org/openlaszlo/trunk@7487 fa20e4f9-1d0a-0410-b5f3-dc9c16b8b17c

---
+++ WEB-INF/lps/server/sc/src/org/openlaszlo/sc/parser/ASTFormalParameterList.java
@@ -0,0 +1,31 @@
+/* ****************************************************************************
+ * ASTFormalParamterList.java
+* ****************************************************************************/
+
+/* J_LZ_COPYRIGHT_BEGIN *******************************************************
+* Copyright 2007 Laszlo Systems, Inc.  All Rights Reserved.                   *
+* Use is subject to license terms.                                            *
+* J_LZ_COPYRIGHT_END *********************************************************/
+
+package org.openlaszlo.sc.parser;
+
+public class ASTFormalParameterList extends SimpleNode {
+    private ASTIdentifier.Type returnType = null;
+
+    public ASTFormalParameterList(int id) {
+        super(id);
+    }
+
+    public ASTFormalParameterList(Parser p, int id) {
+        super(p, id);
+    }
+
+    public ASTIdentifier.Type getReturnType() {
+        return returnType;
+    }
+
+    public void setReturnType(ASTIdentifier.Type type) {
+        this.returnType = type;
+    }
+
+}
