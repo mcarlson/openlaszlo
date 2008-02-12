5ee6febf00484931aef95f1e5f8156c2e7bb17be
bargull <bargull@fa20e4f9-1d0a-0410-b5f3-dc9c16b8b17c>
2008-02-12 17:34:13 +0000

    Change 20080211-bargull-6 by bargull@dell--p4--2-53 on 2008-02-11 22:46:58     in /home/Admin/src/svn/openlaszlo/trunk     for http://svn.openlaszlo.org/openlaszlo/trunk
    Summary: Cleanup for LzLibrary
    
    New Features:
    
    Bugs Fixed: LPP-5419, LPP-4227
    
    Technical Reviewer: hminsky
    QA Reviewer: (pending)
    Doc Reviewer: dda
    
    Documentation:
    
    Release Notes:
    
    Details:
    Only a couple of minor fixes and improvements:
    - added attributes/events to the LzLibrary classes (SWF,DHTML)
    - moved code from LzLibraryCleanup to LzLibrary (LzLibrary#loadfinished(..)) (OOP-thingy)
    - added "loaded"/"loading" states for DHTML (were already supported in SWF)
    - fixed: LzLibrary#destroy(..) did not destroy the library-sprite
    - removed old code in LzLibrary#unload() (SWF) ("delete this.__LZmovieClipRef" is no longer necessary, was for OL3.x)
    - LzLibraryLoader#snippetLoaded(..) did not properly update "loaded"/"loading" states, did not fire "ontimeout"-event, etc.
    - added docs for attributes/methods
    
    
    Tests:
    
    
    
    git-svn-id: http://svn.openlaszlo.org/openlaszlo/trunk@8007 fa20e4f9-1d0a-0410-b5f3-dc9c16b8b17c

--- WEB-INF/lps/lfc/kernel/dhtml/LzLibrary.js
+++ WEB-INF/lps/lfc/kernel/dhtml/LzLibrary.js
@@ -1,7 +1,7 @@
 /**
   * LzLibrary.as
   *
-  * @copyright Copyright 2001-2007 Laszlo Systems, Inc.  All Rights Reserved.
+  * @copyright Copyright 2001-2008 Laszlo Systems, Inc.  All Rights Reserved.
   *            Use is subject to license terms.
   *
   * @topic LZX
@@ -18,6 +18,43 @@ class LzLibrary extends LzNode {
   * @modifiers override 
   */
 static var tagname = 'import';
+
+/** @access private */
+var loaded = false;
+/** @access private */
+var loading = false;
+/** @access private */
+var sprite = null;
+
+/**
+ * A reference to a target file whose content is treated as a 
+ * loadable module.
+ * 
+ * @keywords final
+ * @type string
+ * @access public 
+ */
+var href;
+
+/** 
+ * When set to 'defer', the library will not be loaded until its 
+ * load-method has been called. Otherwise, the library loads 
+ * automatically. 
+ * 
+ * @keywords final
+ * @type string 
+ * @lzxdefault "late"
+ * @access public 
+ */
+var stage = "late";//"late|defer"
+
+/**
+ * Sent when this library has finished loading. 
+ * 
+ * @lzxtype event
+ * @access public
+ */
+var onload = LzDeclaredEvent;
 
 /**
   * @access private
@@ -39,8 +76,19 @@ function init( ) {
     }
 }
 
+/**
+  * @access private
+  */
+function destroy () {
+    if (this.sprite) {
+        this.sprite.destroy();
+        this.sprite = null;
+    }
+    super.destroy.apply(this, arguments);
+}
 
-static var libraries = [];
+/** @access private */
+static var libraries = {};
 
 /**
   * @access private
@@ -49,19 +97,35 @@ static function findLibrary (libname){
     return LzLibrary.libraries[libname];
 }
 
-var loaded = false;
-var loading = false;
-var onload = LzDeclaredEvent;
-
+/**
+  * @access private
+  */
 function toString (){
     return "Library " + this.href + " named " + this.name;
 }
 
 /**
-  * @access private
+  * Loads this library dynamically at runtime. Must only be called 
+  * when stage was set to 'defer'.
+  * 
+  * @access public
   */
 function load (){
+    if (this.loading || this.loaded) {
+        return;
+    }
+    this.loading = true;
     Lz.__dhtmlLoadLibrary(this.href);
+}
+
+/** 
+ * Called by LzLibraryCleanup when this library has finished loading.
+ * 
+ * @access private 
+ */
+function loadfinished (){
+  this.loading = false;
+  if (this.onload.ready) this.onload.sendEvent(true);
 }
 
 /**
@@ -70,20 +134,23 @@ function load (){
   */
 static function __LZsnippetLoaded (url){
     // find the lib with this url
-    var libname = null;
+    var lib = null;
     var libs = LzLibrary.libraries;
     for (var l in libs ) {
         if (libs[l].href == url) {
-            libname = libs[l].name;
+            lib = libs[l];
             break;
         }
     }
-    if (libname == null) {
+    
+    if (lib == null) {
         Debug.error("could not find library with href", url);
+    } else {
+        lib.loaded = true;
+        LzInstantiateView({attrs: {libname: lib.name}, name: "__libraryloadercomplete"}, 1);
+        // Run the queue to instantiate all pending LzInstantiateView calls.
+        canvas.initDone();
     }
-    LzInstantiateView({attrs: {libname: libname}, name: "__libraryloadercomplete"}, 1);
-    // Run the queue to instantiate all pending LzInstantiateView calls.
-    canvas.initDone();
 }
 
 }; // End of LzLibrary
