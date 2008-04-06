9dc3db8d394d7ba9e1367fd93470dbbbe0f51950
dda <dda@fa20e4f9-1d0a-0410-b5f3-dc9c16b8b17c>
2008-04-06 18:17:56 +0000

    Change 20080406-dda-2 by dda@lester.local on 2008-04-06 13:39:47 EDT     in /Users/dda/laszlo/src/svn/openlaszlo/trunk-doc2     for http://svn.openlaszlo.org/openlaszlo/trunk
    Summary: Update navigation, and automatically gen most of navigation
    
    New Features:
    
    Bugs Fixed: LPP-5698
    
    Technical Reviewer: (pending)
    QA Reviewer: (pending)
    Doc Reviewer: liorio (pending)
    
    Documentation:
    
    Release Notes:
    
    Details:
        - Incorporated ruby scripts to generate and check links into doc build:
          Currently, classes.xml and tags.xml are automatically generated by
          navxmlbuilder.rb.  The 'table of contents', toc.xml is not auto-generated,
          since its layout is not generated from sources.  But broken links are
          checked (by checktoc.rb) and gives an error if docs/src/nav/toc.xml needs
          to be updated.
    
        - Updated navigation .xml files to match latest classes and *.html generated.
          In theory, we don't need classes.xml and tags.xml in svn anymore since they
          are autogenerated, but we're keeping them for now in case anyone is missing
          ruby and needs to skip the ruby steps.
    
    Tests:
    
    
    
    git-svn-id: http://svn.openlaszlo.org/openlaszlo/trunk@8573 fa20e4f9-1d0a-0410-b5f3-dc9c16b8b17c

--- docs/src/reference/navbuilder/toccheck.rb
+++ docs/src/reference/navbuilder/toccheck.rb
@@ -6,17 +6,17 @@
 # built on OSX 10.4 with ruby 1.8.6
 # Copyright 2008 Laszlo Systems. Use according to license terms. 
 
-DEBUGLEVEL = 0
+require 'getoptlong'
+require 'rdoc/usage'
 
-REFDOC_DIR = '../../../reference'
+$debuglevel = 0
+$indir = '../../../reference/'   # always terminated by slash
 
 def debug ( lev, str )
-  if (lev <= DEBUGLEVEL) then
+  if (lev <= $debuglevel) then
      $stdout.puts str + "\n"
   end
 end
-# only appears if DEBUGLEVEL set to 1 or higher
-debug(1, "debug level is set to " + DEBUGLEVEL.to_s)
   
 def for_line(filename)
   tag = nil;
@@ -29,18 +29,41 @@ def for_line(filename)
 end
 
 def check_link(fromname, refname)
-  if (!File.exist?(REFDOC_DIR + '/' + refname)) then
-    $stderr.puts(refname + ": referenced from " + fromname + " does not exist")
+  result = true
+  if (!File.exist?($indir + refname)) then
+    $stderr.puts("Error: broken link in doc navigation: " + refname + " referenced from " + fromname + " does not exist")
     basename = refname.sub(/[.][^.]*/,'').sub(/[+].*/,'')
-    Dir.glob(REFDOC_DIR + '/' + basename + '*' ).sort.each { |altname|
-      altname = altname.sub(REFDOC_DIR + '/', '')
-      $stderr.puts('    ' + altname + '?')
+    Dir.glob($indir + basename + '*' ).sort.each { |altname|
+      altname = altname.sub($indir, '')
+      $stderr.puts('    maybe use ' + altname + '?')
+      result = false
     }
   end
+  return result
 end
 
+opts = GetoptLong.new(
+  [ '--help', GetoptLong::NO_ARGUMENT ],
+  [ '--inputdir', GetoptLong::REQUIRED_ARGUMENT ],
+  [ '--debug', GetoptLong::REQUIRED_ARGUMENT ]
+)
+
+opts.each do |opt, arg|
+  case opt
+    when '--help'
+      RDoc::usage
+    when '--inputdir'
+      $indir = arg + '/';
+    when '--debug'
+      $debuglevel = arg.to_i
+  end
+end
+# only appears if $debuglevel set to 1 or higher
+debug(1, "debug level is set to " + $debuglevel.to_s)
+
 todo_count = 0
-for_line(REFDOC_DIR + '/toc.xml') { | line |
+ecode = true
+for_line($indir + 'toc.xml') { | line |
    if (line =~ /TODO/) then
       todo_count = todo_count + 1
    end
@@ -49,10 +72,17 @@ for_line(REFDOC_DIR + '/toc.xml') { | line |
    end
    if (line =~ /<item.*href=/) then
       filename = line.sub(/^.*href="([^"]*)".*$/, '\1')
-      check_link('toc.xml', filename)
+      if (!check_link('toc.xml', filename))
+         ecode = false
+      end
    end
 }
 
 if (todo_count != 0) then
   $stderr.puts('' + todo_count.to_s + ' TODOs found in toc.xml')
+end
+
+if (!ecode) then
+   $stderr.puts('Error: docs/src/nav/toc.xml is out of date, see broken link messages above')
+   exit(1)
 end
