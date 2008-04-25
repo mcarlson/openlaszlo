818b88520b0d3a7fe17ffc4cabe9f3414b3f4b4f
ptw <ptw@fa20e4f9-1d0a-0410-b5f3-dc9c16b8b17c>
2008-04-25 00:03:53 +0000

    Change 20080424-ptw-e by ptw@dueling-banjos.local on 2008-04-24 19:41:44 EDT     in /Users/ptw/OpenLaszlo/ringding-2     for http://svn.openlaszlo.org/openlaszlo/trunk
    Summary: Clean up debugger reference pages
    
    Bugs Fixed:
    LPP-5589 'debug reference page problems'
    
    Technical Reviewer: lou@louiorio.com (pending)
    
    Details:
        Fixed up documentation, added an example, fixed broken links,
        added parameter declarations, re-factored branches of
        compile-time switch to reduce doc cases.
    
    Tests:
        Generated reference manual and visually inspected
    
    
    
    git-svn-id: http://svn.openlaszlo.org/openlaszlo/trunk@8849 fa20e4f9-1d0a-0410-b5f3-dc9c16b8b17c

--- WEB-INF/lps/lfc/debugger/platform/dhtml/LzMonitor.js
+++ WEB-INF/lps/lfc/debugger/platform/dhtml/LzMonitor.js
@@ -2,21 +2,22 @@
 
 /// Platform-specific monitor support
 
-//---
-// Monitor a property of an object
-//
-// @param Object who: the object whose property to monitor
-// @param String what: the name of the property to monitor
-//
-// Will monitor the named property of the object and print a message
-// to the Debug console each time it is modified.  The message
-// includes a timestamp, the function that modified the property, the
-// object and property, and the old and new values.
-//
-// If backtraces are enabled, inspecting the message will reveal the
-// call chain that caused the modification.
-
-//---
+/**
+ * Monitor a property of an object
+ *
+ * Will monitor the named property of the object and print a message
+ * to the Debug console each time it is modified.  The message
+ * includes a timestamp, the function that modified the property, the
+ * object and property, and the old and new values.
+ *
+ * If backtraces are enabled, inspecting the message will reveal the
+ * call chain that caused the modification.
+ *
+ * @param Object who: the object whose property to monitor
+ * @param String what: the name of the property to monitor
+ *
+ * @see Debug.unmonitor
+ */
 Debug.monitor = function(who, what) {
   var o = who[what];
   var s = function (n) {
@@ -61,10 +62,11 @@ Debug.monitor = function(who, what) {
 
 /**
   * Stop monitoring a property of an object
+  *
   * @param Object who: the object whose property to not monitor
   * @param String what: the name of the property to not monitor
-  * 
-  * See also: Debug.monitor
+  *
+  * @see Debug.monitor
   */
 Debug.unmonitor = function(who, what) {
   var o = who[what];
@@ -75,6 +77,6 @@ Debug.unmonitor = function(who, what) {
   who[what] = o;
 }
 
-// Copyright 2001-2006 Laszlo Systems, Inc.  All Rights Reserved.  Use
+// Copyright 2001-2006, 2008 Laszlo Systems, Inc.  All Rights Reserved.  Use
 // is subject to license terms.
 
