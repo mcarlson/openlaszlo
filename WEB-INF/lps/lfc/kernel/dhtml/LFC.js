f5285c87de478faa57cad0b1ffe896cb8ed89c45
pbr <pbr@fa20e4f9-1d0a-0410-b5f3-dc9c16b8b17c>
2008-04-28 19:02:22 +0000

    Change 20080428-Philip-a by Philip@Philip-DC on 2008-04-28 13:48:14 EDT      in /cygdrive/f/laszlo/svn/src/svn/openlaszlo/trunk      for http://svn.openlaszlo.org/openlaszlo/trunk
    Summary:  fix getTimer() for dhtml
    
    New Features:
    
    Bugs Fixed: LPP-5887
    
    Technical Reviewer: ptw
    QA Reviewer: (pending)
    Doc Reviewer: (pending)
    
    Documentation:
    
    Release Notes:
    
    Details:
    The global method getTimer() got removed in a recent changeset. This restores getTimer() and it calls the static method LzTimeKernel.getTimer().
    
    Tests:
    No javascript error when calendar demo starts up in dhtml:
    http://localhost:8080/trunk/demos/calendar/calendar.lzx?lzr=dhtml&lzt=html
    
    Files:
    M      WEB-INF/lps/lfc/kernel/dhtml/LFC.js
    
    Changeset: http://svn.openlaszlo.org/openlaszlo/patches/20080428-Philip-a.tar
    
    
    
    git-svn-id: http://svn.openlaszlo.org/openlaszlo/trunk@8874 fa20e4f9-1d0a-0410-b5f3-dc9c16b8b17c

--- WEB-INF/lps/lfc/kernel/dhtml/LFC.js
+++ WEB-INF/lps/lfc/kernel/dhtml/LFC.js
@@ -11,6 +11,13 @@
 // Resource library
 var LzResourceLibrary = {};
 
+{
+#pragma "profile=false"
+    var getTimer = function() {
+        return LzTimeKernel.getTimer();
+    }
+}
+
 // FIXME: [2006-03-24 ptw] This will be wrong when there are modules.
 // In the module world $modules.runtime = window and global =
 // $modules.user
