mcarlson/openlaszlo

commit c6457ff7c40af109424050eb0c5c0e4021fa914f

Change 20090519-ptw-1 by ptw@dueling-banjos.home on 2009-05-19 18:28:01 EDT     in /Users/ptw/OpenLaszlo/trunk     for http://svn.openlaszlo.org/openlaszlo/trunk
Summary: Make the component debugger window work in DHTML

Bugs Fixed: LPP-7700 Debugger should be resizeable and moveable in DHTML

Technical Reviewer: max (Message-ID: <4A13580B.4010306@openlaszlo.org>), hminsky (pending)
QA Reviewer: mdemmon (pending)

Details:
    Expand the "DebuggerWindowConsoleBridge" to work on all platforms
    so that all platforms work the same.  The component window either
    attaches itself to the debugger (if there is a <debug> tag) or the
    debugger will create a component window.

    For the DHTML bridge, some additional markup is used (because it
    is available) and the evaluator is implemented on the platform
    rather than by calling back to the server.

    The simple evaluator for 'path' expressions is moved into the
    bootstrap console so that it can be shared on platforms that do
    not support eval.

    Each of the compiler backends will either output the <debug> tag
    specified in the user program to instantiate the console window,
    or insert the appropriate code to cause the debugger to
    instantiate a default window.

    Removed the DHTML iframe-based debug console -- no longer needed.

Tests:
    Tried debugging something in all 3 runtimes



git-svn-id: http://svn.openlaszlo.org/openlaszlo/trunk@13964 fa20e4f9-1d0a-0410-b5f3-dc9c16b8b17c

WEB-INF/lps/lfc/compiler/LzBootstrapDebugService.lzs
@@ -13,11 +13,9 @@
 /**
  * The Debug Console interface
  *
- * This interface defines the protocol between the debugger and the console
+ * This abstract class defines the protocol between the debugger and the console
  *
  * @access private
- *
- * @devnote [2008-11-25 ptw] Should really just be an interface
  */
 class LzDebugConsole {
 
@@ -36,12 +34,28 @@ class LzDebugConsole {
    *
    * @access private
    */
-  function addText (msg) {};
+  /** @access private */
+  function addText (msg) {
+    var str;
+    try {
+      if (msg && msg['toHTML'] is Function) {
+        str = msg.toHTML();
+      } else {
+        str = String(msg)['toHTML']();
+      }
+    } catch (e) {
+      Debug.log('ERROR: ' + e + ' in addText(' + msg + ')');
+      str = '' + msg;
+    };
+    this.addHTMLText(str);
+  };
+
   /**
    * Clears the console
    * @access private
    */
   function clear () {};
+
   /**
    * Adds output to the console, potentially marking it with a font or
    * color to indicate that it is input, not output.
@@ -53,6 +67,7 @@ class LzDebugConsole {
    * @access private
    */
   function echo (str, newLine:Boolean=true) {};
+
   /**
    * Add HTML output to the console
    *
@@ -61,6 +76,7 @@ class LzDebugConsole {
    * escaped.
    */
   function addHTMLText (msg) {};
+
   /**
    * Make a 'presentation' of an object
    *
@@ -80,18 +96,65 @@ class LzDebugConsole {
    * @access private
    */
   function makeObjectLink (rep:String, id, attrs=null):String { return undefined; };
+
+  /**
+   * Simple "path" expressions will only have symbols, `.`, or `[numbers]`
+   * @access private
+   */
+  const SimpleExprPattern = new RegExp('^\\s*([$_A-Za-z][$\\w]*)((\\s*\\.\\s*[$_A-Za-z][$\\w]*)|(\\s*\\[\\s*\\d+\\s*\\]))*\\s*$');
+  /**
+   * Now pull out just the element 'selectors', so you can walk down them
+   * @access private
+   */
+  const ElementPattern = new RegExp('([$_A-Za-z][$\\w]*)|(\\d+)', 'g');
+
+  /** @access private */
+  function isSimpleExpr(expr:String):Boolean {
+    return (expr.match(this.SimpleExprPattern));
+  }
+
+  /** @access private */
+  function evalSimpleExpr(expr:String) {
+    var parts = expr.match(this.ElementPattern);
+    var val = globalValue(parts[0]);
+    for (var i = 1, l = parts.length; i < l; i++) {
+      val = val[parts[i]];
+    }
+    return val;
+  }
+
   /**
    * Evaluate an expression and call Debug.displayResult on the result
    * (or Debug.error if the evaluation causes an error).
    *
-   * This is part of the console protocol because it may require using
-   * the console link to compile the expression to be evaluated, which
-   * is then loaded and executed.  The result is then displayed by
-   * calling back to the debugger `displayResult` method
+   * The bootstrap version assumes the platform does not have an
+   * evaluator.  It evaluates simple member/index paths 'by hand',
+   * otherwise it punts.  Platform-specific bridges override this
+   * method to use either the platform evaluator, or, if tethered,
+   * the remote server to compile/load/eval the expression.
    *
    * @access private
    */
-  function doEval (expr:String) {};
+  function doEval (expr:String) {
+    if (this.isSimpleExpr(expr)) {
+      var simple = true;
+      try {
+        var val = this.evalSimpleExpr(expr);
+        // If we got no value, maybe the expression was not simple
+        if (val === void 0) {
+          simple = false;
+        }
+      } catch (e) {
+        // If we get an error, surely the expression must not be simple
+        simple = false;
+      }
+    }
+    if (simple) {
+      Debug.displayResult(val);
+    } else {
+      Debug.warn("Unable to evaluate %s", expr);
+    } 
+  }
 };
 
 
@@ -105,7 +168,7 @@ class LzDebugConsole {
  *
  * @access private
  */
-class LzBootstrapDebugConsole extends LzDebugConsole /* implements  LzDebugConsole */ {
+class LzBootstrapDebugConsole extends LzDebugConsole {
 
   function LzBootstrapDebugConsole () {
     this.saved_msgs = new Array;
@@ -127,27 +190,22 @@ class LzBootstrapDebugConsole extends LzDebugConsole /* implements  LzDebugConso
         str = String(msg)['toHTML']();
       }
     } catch (e) {
+      Debug.log('ERROR: ' + e + ' in addText(' + msg + ')');
       str = '' + msg;
     };
     if ($dhtml) {
-      try {
-        if (navigator.platform == 'rhino') {
+      // `ant lztest` depends on rhino output
+      if (navigator.platform == 'rhino') {
+        try {
           print(str);
           return;
-        }
-      } catch (e) {};
+        } catch (e) {};
+      }
     }
     // We know this is just going to capture the raw message for
     // replay later
     this.addHTMLText(str);
   };
-
-  /**
-   * Doc'd on interface
-   * Bootstrap version
-   * @access private
-   */
-  override function clear () { };
 
   /**
    * Doc'd on interface
@@ -190,7 +248,7 @@ class LzBootstrapDebugConsole extends LzDebugConsole /* implements  LzDebugConso
   override function doEval (expr:String) {
     // Bootstrap implementation
     try {
-      Debug.displayResult(globalValue(expr));
+      super.doEval(expr);
     } catch (e) {
       Debug.error(e);
     }

WEB-INF/lps/lfc/debugger/platform/dhtml/LzDebug.js
@@ -152,19 +152,6 @@ class LzDHTMLDebugService extends LzDebugService {
     for (var k in copy) {
       this[k] = base[k];
     }
-
-    // Make the real console.  In DHTML we can do this in the
-    // constructor, rather than in makeDebugWindow, because the
-    // console is implemented as an HTML iframe that exists before the
-    // app is loaded
-    var debugFrame;
-    try {
-      debugFrame = window.parent.frames['LaszloDebugger'];
-    }
-    catch (e) {};
-    if (debugFrame) {
-      this.attachDebugConsole(new LzDHTMLDebugConsole(debugFrame));
-    }
   };
 
   /**
@@ -205,12 +192,6 @@ class LzDHTMLDebugService extends LzDebugService {
    * Called last thing by the compiler when the app is completely loaded.
    */
   function makeDebugWindow () {
-    // If we didn't succeed in attaching the debug console in
-    // construct, try now
-    if ((! (this.console is LzDHTMLDebugConsole)) &&
-        (navigator.platform != 'rhino')) {
-      this.attachDebugConsole(new LzDHTMLDebugConsole(this.createDebugIframe()));
-    }
     for (var n in __ES3Globals) {
       var p = __ES3Globals[n];
       try {
@@ -222,6 +203,21 @@ class LzDHTMLDebugService extends LzDebugService {
         //        Debug.debug("Can't name %w", name);
       }
     }
+    // Make the real console.  This is only called if the user code
+    // did not actually instantiate a <debug /> tag
+    if (global['lzconsoledebug'] == 'true') {
+      // Open the remote debugger socket
+      this.attachDebugConsole(new LzFlashRemoteDebugConsole());
+    } else {
+      // This will attach itself, once it is fully initialized.
+      new lz.LzDebugWindow();
+    }
+//     // If we didn't succeed in attaching the debug console in
+//     // construct, try now
+//     if ((! (this.console is LzDHTMLDebugConsole)) &&
+//         (navigator.platform != 'rhino')) {
+//       this.attachDebugConsole(new LzDHTMLDebugConsole(this.createDebugIframe()));
+//     }
   };
 
   /**

WEB-INF/lps/lfc/debugger/Library.lzs
@@ -9,7 +9,7 @@
   * In some runtimes the Debug object is subclassed for added
   * functionality by the component/debug library
   *
-  * @copyright Copyright 2001-2008 Laszlo Systems, Inc.  All Rights Reserved.
+  * @copyright Copyright 2001-2009 Laszlo Systems, Inc.  All Rights Reserved.
   *            Use is subject to license terms.
   *
   * @access private
@@ -31,8 +31,10 @@
   // Platform-specific debugger
   if ($dhtml) {
     #include "debugger/platform/dhtml/LzDebug.js"
+    #include "debugger/platform/dhtml/LzDebuggerWindowConsoleBridge.js"
   } else if ($as2) {
     #include "debugger/platform/swf/LzDebug.as"
+    #include "debugger/platform/swf/LzDebuggerWindowConsoleBridge.as"
   } else if ($as3) {
     #include "debugger/platform/swf9/LzDebug.as"
     #include "debugger/platform/swf9/LzFlashRemote.as"

WEB-INF/lps/lfc/debugger/LzDebug.lzs
@@ -258,6 +258,9 @@ dynamic class LzDebugService extends LzBootstrapDebugService with LzFormatter {
     for (var i = 0; i < sml; i++) {
       this.console.addText(sm[i]);
     }
+    // Returns the debugger bridge for calling from the server back to
+    // the client
+    return this;
   }
 
   /**

WEB-INF/lps/lfc/debugger/platform/dhtml/LzDebuggerWindowConsoleBridge.js
@@ -1,0 +1,93 @@
+/* -*- mode: JavaScript; c-basic-offset: 2; -*- */
+
+/*
+ * Bridge to forward console API calls to the GUI debugger window view
+ *
+ * @copyright Copyright 2001-2009 Laszlo Systems, Inc.  All Rights Reserved.
+ *            Use is subject to license terms.
+
+ */
+
+class LzDebuggerWindowConsoleBridge extends LzDebugConsole {
+
+  var DebugWindow;
+
+  function LzDebuggerWindowConsoleBridge (view) {
+    super();
+    this.DebugWindow = view;
+  }
+
+  /** @access private */
+  var __reNewline = RegExp('&#xa;|&#xA;|&#10;|\\n', 'g');
+
+  /**
+   * @access private
+   */
+  override function addHTMLText (str) {
+    this.DebugWindow.addHTMLText(str.replace(this.__reNewline, '<br />'));
+  }
+
+  /**
+   * @access private
+   */
+  override function clear () {
+    this.DebugWindow.clearWindow();
+  };
+
+  /**
+   * @access private
+   */
+  override function echo (str, newLine:Boolean=true) {
+    // This color should be a debugger constant -- maybe there should
+    // be LzEcho extends LzSourceMessage
+    this.addHTMLText('<span style="color: #00cc00">' + str + '</span>' + (newLine?'\n':''));
+  }
+
+  /**
+   * @access private
+   */
+  override function makeObjectLink (rep:String, id, attrs=null):String {
+    var color = (attrs && attrs['color']) ? attrs.color : '#0000ff';
+    var decoration = (attrs && attrs['type']) ? 'none' : 'underline';
+    if (id != null) {
+      var obj = Debug.ObjectForID(id);
+      var tip = Debug.formatToString("Inspect %0.32#w", obj).toString().toHTML();
+      // This ends up being inserted into the debugger output div.
+      // `$modules.lz` should be visible as a global there
+      return '<span style="cursor: pointer; text-decoration: ' + decoration + '; color: ' + color + '" title="' + tip + '" onclick="javascript:$modules.lz.Debug.displayObj(' + id + ')">' + rep +"</span>";
+    }
+    return rep;
+  };
+
+
+  /**
+   * @access private
+   */
+  override function doEval (expr:String) {
+#pragma "warnUndefinedReferences=false"
+    try {
+      with (Debug.environment) {
+        // Evaluate as expression first
+        var value = window.eval('(' + expr + ')');
+      }
+      Debug.displayResult(value);
+    }
+    catch (e) {
+      if (! (e is SyntaxError)) {
+        Debug.error("%s", e);
+        return;
+      }
+
+      // Not an expression, see if it is a statement
+      try {
+        with (Debug.environment) {
+          var value = window.eval(expr);
+        }
+        Debug.displayResult(value);
+      }
+      catch (e) {
+        Debug.error("%s", e);
+      }
+    }
+  };
+}

WEB-INF/lps/server/src/org/openlaszlo/compiler/DHTMLWriter.java
@@ -380,14 +380,18 @@ class DHTMLWriter extends ObjectWriter {
 
         boolean debug = mProperties.getProperty("debug", "false").equals("true");
 
-        // This indicates whether the user's source code already manually invoked
-        // <debug> to create a debug window. If they didn't explicitly call for
-        // a debugger window, instantiate one now by passing 'true' to __LzDebug.startDebugWindow()
-        boolean makedebugwindow =  !mEnv.getBooleanProperty(mEnv.USER_DEBUG_WINDOW);
-
         // Bring up a debug window if needed.
-        if (debug && makedebugwindow) {
-            addScript("__LzDebug.makeDebugWindow()");
+        if (debug) {
+            boolean userSpecifiedDebugger = mEnv.getBooleanProperty(mEnv.USER_DEBUG_WINDOW);
+            // This indicates whether the user's source code already manually invoked
+            // <debug> to create a debug window. If they didn't explicitly call for
+            // a debugger window, instantiate one now by calling _LZDebug.makeDebugWindow()
+            if (userSpecifiedDebugger) {
+                addScript(mEnv.getProperty(mEnv.DEBUGGER_WINDOW_SCRIPT));
+            } else {
+                // Create debugger window with default init options
+                addScript("Debug.makeDebugWindow()");
+            }
         }
 
         // Tell the canvas we're done loading.

WEB-INF/lps/server/src/org/openlaszlo/compiler/DebugCompiler.java
@@ -45,8 +45,6 @@ class DebugCompiler extends ViewCompiler {
         // a remote connection instead. 
         if (!mEnv.getBooleanProperty(mEnv.DEBUG_PROPERTY)
             || mEnv.getBooleanProperty(mEnv.USER_DEBUG_WINDOW)
-            // No debug window in DHTML -- it is in its own iframe.
-            || mEnv.isDHTML()
             || mEnv.getBooleanProperty(mEnv.CONSOLEDEBUG_PROPERTY)) {
             return;
         } else {

commit f434cb2119429cd081722fc7088ad41bff6398d9

Merged revisions 14007 via svnmerge from http://svn.openlaszlo.org/openlaszlo/branches/4.4
........
  r14007 | ptw | 2009-05-26 12:23:29 -0400 (Tue, 26 May 2009) | 25 lines
  
  Change 20090526-ptw-L by ptw@dueling-banjos.home on 2009-05-26 12:15:44 EDT
      in /Users/ptw/OpenLaszlo/4.4
      for http://svn.openlaszlo.org/openlaszlo/branches/4.4
  
  Summary: Make `ant lztest` work again
  
  Bugs Fixed: LPP-8227  Nightly builds failing megatest after r13964
  
  Technical Reviewer: hminsky (pending)
  QA Reviewer: fellenberger@laszlosystems.com (pending)
  
  Details:
      lztest-node-options: Remove explicit `debug=true` which causes the
      component debugger window to be loaded -- this is not needed for
      lztest and just causes a lot of unnecessary warnings
  
      dhtml/LzDebug: Remove unnecessary override
  
      LzBootstrapDebugService: insert Rhino hook to capture debug
      output, remove unneccesary override.
  
  Tests:
      ant lztest, smokecheck on all platforms
........


git-svn-id: http://svn.openlaszlo.org/openlaszlo/trunk@14013 fa20e4f9-1d0a-0410-b5f3-dc9c16b8b17c

WEB-INF/lps/lfc/compiler/LzBootstrapDebugService.lzs
@@ -44,9 +44,17 @@ class LzDebugConsole {
         str = String(msg)['toHTML']();
       }
     } catch (e) {
-      Debug.log('ERROR: ' + e + ' in addText(' + msg + ')');
       str = '' + msg;
     };
+    if ($dhtml) {
+      // `ant lztest` depends on rhino output
+      if (navigator.platform == 'rhino') {
+        try {
+          print(str);
+          return;
+        } catch (e) {};
+      }
+    }
     this.addHTMLText(str);
   };
 
@@ -173,39 +181,6 @@ class LzBootstrapDebugConsole extends LzDebugConsole {
   function LzBootstrapDebugConsole () {
     this.saved_msgs = new Array;
   }
-
-  /**
-   * Doc'd on interface
-   * Bootstrap version
-   * @access private
-   *
-   * @devnote Should probably be called addMessage
-   */
-  override function addText (msg) {
-    var str;
-    try {
-      if (msg && msg['toHTML'] is Function) {
-        str = msg.toHTML();
-      } else {
-        str = String(msg)['toHTML']();
-      }
-    } catch (e) {
-      Debug.log('ERROR: ' + e + ' in addText(' + msg + ')');
-      str = '' + msg;
-    };
-    if ($dhtml) {
-      // `ant lztest` depends on rhino output
-      if (navigator.platform == 'rhino') {
-        try {
-          print(str);
-          return;
-        } catch (e) {};
-      }
-    }
-    // We know this is just going to capture the raw message for
-    // replay later
-    this.addHTMLText(str);
-  };
 
   /**
    * Doc'd on interface

WEB-INF/lps/lfc/debugger/platform/dhtml/LzDebug.js
@@ -43,32 +43,6 @@ class LzDHTMLDebugConsole extends LzBootstrapDebugConsole {
   };
 
   /**
-   * @access private
-   */
-  override function addText (msg) {
-    var str;
-    try {
-      if (msg && msg['toHTML'] is Function) {
-        str = msg.toHTML();
-      } else {
-        str = String(msg).toHTML();
-      }
-    } catch (e) {
-      str = '' + msg;
-    };
-    try {
-      this.addHTMLText(str);
-    } catch (e) {
-      try {
-        if (navigator.platform == 'rhino') {
-          print(str);
-          return;
-        }
-      } catch (e) {};
-    }
-  };
-
-  /**
    * Echo to the console in the debugger font
    *
    * @param String str: what to echo

test/lztest/lztest-node-options.lzx
@@ -1,4 +1,4 @@
-<canvas debug="true">
+<canvas>
 <!--
   covers-tags : lznode smoketest
 -->
@@ -30,4 +30,4 @@
     ]]>
   </script>
 </canvas>
-<!-- Copyright 2006 Laszlo Systems, Inc.  All Rights Reserved.  Use subject to license terms. -->
+<!-- Copyright 2006, 2009 Laszlo Systems, Inc.  All Rights Reserved.  Use subject to license terms. -->