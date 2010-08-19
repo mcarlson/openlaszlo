4abc4476020b62d46d382f86fea5b49be9684f38
max <max@fa20e4f9-1d0a-0410-b5f3-dc9c16b8b17c>
2010-08-19 21:36:44 +0000

    Should have gone with r17320
    git-svn-id: http://svn.openlaszlo.org/openlaszlo/trunk@17321 fa20e4f9-1d0a-0410-b5f3-dc9c16b8b17c

---
+++ lps/includes/source/iframestub.js
@@ -0,0 +1,46 @@
+/* X_LZ_COPYRIGHT_BEGIN ***************************************************
+* Copyright 2001-2010 Laszlo Systems, Inc.  All Rights Reserved.          *
+* Use is subject to license terms.                                        *
+* X_LZ_COPYRIGHT_END ******************************************************/
+try {
+    if (lz) {}
+} catch (e) {
+    lz = {};
+}    
+
+// retrieve our frame id from the lz.embed namespace in the loaded page
+lz.frameid = parent.lz.embed.iframemanager.getIDFromWindow(this);
+
+// send an event to the html component controlling this frame
+lz.sendEvent = function(name, value) {
+    var args = [].slice.call(arguments);
+    // prepend our iframe id
+    args.unshift(lz.frameid);
+    //console.log('calling iframemanager.asyncCallback with args',args);
+    var iframemanager = parent.lz.embed.iframemanager;
+    // Send an asynchronous callback/event
+    iframemanager.asyncCallback.apply(iframemanager, args);
+}
+
+//console.log('found id', lz._iframeid);
+try {
+    if (lz) {}
+} catch (e) {
+    lz = {};
+}    
+
+// retrieve our frame id from the lz.embed namespace in the loaded page
+lz.frameid = parent.lz.embed.iframemanager.getIDFromWindow(this);
+
+// send an event to the html component controlling this frame
+lz.sendEvent = function(name, value) {
+    var args = [].slice.call(arguments);
+    // prepend our iframe id
+    args.unshift(lz.frameid);
+    //console.log('calling iframemanager.asyncCallback with args',args);
+    var iframemanager = parent.lz.embed.iframemanager;
+    // Send an asynchronous callback/event
+    iframemanager.asyncCallback.apply(iframemanager, args);
+}
+
+//console.log('found id', lz._iframeid);
